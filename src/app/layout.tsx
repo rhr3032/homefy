389f02cd7dbd117220f651e9f4b2058862d3d917
rhr3032 <raisulhasanrafi3032@gmail.com>
2026-03-28 19:35:22 +0600

    Remove old favicon and update layout component for improved structure and readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,30 @@
-import type { Metadata } from 'next'
-import { Bricolage_Grotesque } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/Layout/Header'
-import Footer from '@/components/Layout/Footer'
-import { ThemeProvider } from 'next-themes'
+import type { Metadata } from 'next';
+import { Bricolage_Grotesque } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Layout/Header';
+import Footer from '@/components/Layout/Footer';
+import { ThemeProvider } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
-import SessionProviderComp from '@/components/nextauth/SessionProvider'
+import SessionProviderComp from '@/components/nextauth/SessionProvider';
 
-const font = Bricolage_Grotesque({ subsets: ["latin"] });
+const font = Bricolage_Grotesque({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Homefy - A Real Estate Platform for Modern Living',
-  description: 'Discover your dream home with Homefy, the premier real estate platform for modern living. Explore a curated selection of properties, from luxury villas to cozy apartments, all designed to elevate your lifestyle. With user-friendly search tools, expert guidance, and a commitment to excellence, Homefy connects you with the perfect property that suits your needs and aspirations. Start your journey to finding the ideal home today with Homefy.',
-}
+  description:
+    'Discover your dream home with Homefy, the premier real estate platform for modern living. Explore a curated selection of properties, from luxury villas to cozy apartments, all designed to elevate your lifestyle. With user-friendly search tools, expert guidance, and a commitment to excellence, Homefy connects you with the perfect property that suits your needs and aspirations. Start your journey to finding the ideal home today with Homefy.',
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico?v=2" sizes="any" />
+      </head>
       <body className={`${font.className} bg-white dark:bg-black antialiased`}>
         <NextTopLoader color="#07be8a" />
         <SessionProviderComp>
-          <ThemeProvider
-            attribute='class'
-            enableSystem={true}
-            defaultTheme='light'>
+          <ThemeProvider attribute="class" enableSystem defaultTheme="light">
             <Header />
             {children}
             <Footer />
@@ -35,5 +32,5 @@ export default function RootLayout({
         </SessionProviderComp>
       </body>
     </html>
-  )
+  );
 }
