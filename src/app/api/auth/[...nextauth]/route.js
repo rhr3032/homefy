dd3a1d1b118e3c68f549f8cb0462cf5459b993b4
rhr3032 <raisulhasanrafi3032@gmail.com>
2026-03-28 14:08:16 +0600

    comment out Google and GitHub providers in authentication setup

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,14 +6,14 @@ import GitHubProvider from 'next-auth/providers/github';
 const handler = NextAuth({
   site: process.env.NEXTAUTH_URL || 'http://localhost:3000',
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     CredentialsProvider({
       name: 'credentials',
       credentials: {
